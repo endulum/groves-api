1b353d919caead664352f31ddd10e4a82a38e629
endulum <71853916+endulum@users.noreply.github.com>
2024-11-27 22:34:36 -0800

    add ReplyRating view, get rid of communityId field in Reply schema

--- prisma/sql/ReplyRating.sql
+++ prisma/sql/ReplyRating.sql
@@ -1,15 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `communityId` on the `Reply` table. All the data in the column will be lost.
-
-*/
--- DropForeignKey
-ALTER TABLE "Reply" DROP CONSTRAINT "Reply_communityId_fkey";
-
--- AlterTable
-ALTER TABLE "Reply" DROP COLUMN "communityId";
-
 CREATE OR REPLACE VIEW "ReplyRating" AS SELECT
   "votedReplies".id as "replyId", 
   "votedReplies".upvotes as upvotes, 
