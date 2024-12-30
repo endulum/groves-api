3d4094adbcfe964516f54182b73622a951df142c
endulum <71853916+endulum@users.noreply.github.com>
2024-12-30 13:44:15 -0800

    log input errors when in dev

--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -4,12 +4,15 @@ import { validationResult } from 'express-validator';
 export const validate = asyncHandler(async (req, res, next) => {
   const errorsArray = validationResult(req).array();
   if (errorsArray.length > 0) {
+    const errors = errorsArray.map((error) => ({
+      value: 'value' in error ? error.value : '',
+      msg: error.msg,
+      path: 'path' in error ? error.path : '',
+    }));
+    // eslint-disable-next-line no-console
+    if (process.env.ENV === 'development') console.dir(errors, { depth: null });
     res.status(400).json({
-      errors: errorsArray.map((error) => ({
-        value: 'value' in error ? error.value : '',
-        msg: error.msg,
-        path: 'path' in error ? error.path : '',
-      })),
+      errors,
     });
     return;
   }
